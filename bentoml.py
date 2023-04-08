5c43bd27fac5ad181264d80e03026bdb0d9329b4
taniacsilva <106198492+taniacsilva@users.noreply.github.com>
2023-04-08 18:56:20 +0100

    rename to bentoml.py

---
+++ bentoml.py
@@ -0,0 +1,19 @@
+# Import Libraries
+import bentoml
+from XGBoost_model import *
+
+# XGBoost Model
+eta = 0.1
+md = 3 
+ch = 6
+
+args = parse_arguments()
+data = data_preparation(args.file_name)
+set_used = split_train_val_test(data)
+X_full_train, X_test, dv = one_hot_enconding(set_used[6], set_used[2])
+features = dv.get_feature_names_out()
+model, y_pred, auc, output_string, scores, key_eta, key_md, key_ch =  xgb_model(X_full_train, set_used[7], features, X_test, set_used[5], eta, md, ch)
+
+# Bento ML
+bentoml.xgboost.save_model("credit_risk_model", model, custom_objects={"dictVectorizer":dv})
+print(bentoml.xgboost.save_model("credit_risk_model", model))
